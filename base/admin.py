e0fa17312b409075013d04d8b5328685fa1c8f1d
Athika Priya <athikapriya1997@gmail.com>
2026-03-11 22:44:10 +0600

    Remove all models from base app and unregister from admin

--- base/admin.py
+++ base/admin.py
@@ -1,15 +1,4 @@
 # third party imports
 from django.contrib import admin
 
-# local imports
-from .models import User, JobCompany, JobCategory, Job, Application, SavedJob, Skill, CandidateProfile, EmployerProfile
 
-admin.site.register(User)
-admin.site.register(JobCompany)
-admin.site.register(JobCategory)
-admin.site.register(Job)
-admin.site.register(Application)
-admin.site.register(SavedJob)
-admin.site.register(Skill)
-admin.site.register(CandidateProfile)
-admin.site.register(EmployerProfile)
