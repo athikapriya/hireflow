5d55f406fec8b43c3ae51452390503251c0b281c
Athika Priya <athikapriya1997@gmail.com>
2026-03-11 23:17:01 +0600

    Register Job, JobCategory, and Skill models in admin

---
+++ jobs/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Job, JobCategory, Skill
+
+
+# Register your models here.
+admin.site.register(Job)
+admin.site.register(JobCategory)
+admin.site.register(Skill)
