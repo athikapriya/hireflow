0c7b3e33ab4572f51f1d41b7ed41c38b3fb5fe6c
Athika Priya <athikapriya1997@gmail.com>
2026-03-30 18:41:20 +0600

    Fix Cloudinary setup

--- hireflow/settings.py
+++ hireflow/settings.py
@@ -14,12 +14,10 @@ from pathlib import Path
 import os
 from decouple import config
 from dotenv import load_dotenv
-load_dotenv(os.path.join(BASE_DIR, '.env')) 
 
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env')) 
 
 
 # Quick-start development settings - unsuitable for production
