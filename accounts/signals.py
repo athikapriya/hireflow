0b6b30159033640a196eb403f0d1e97eea61d3a4
Athika Priya <athikapriya1997@gmail.com>
2026-04-06 01:25:29 +0600

    Update user post-save signal to trigger Celery welcome email

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,16 +5,16 @@ from rest_framework.authtoken.models import Token
 from .models import User
 
 
+from .tasks import send_welcome_email
+
 @receiver(post_save, sender=User)
 def user_created_handler(sender, instance, created, **kwargs):
-
     if created:
         if instance.role == 'candidate' and not instance.designation:
             User.objects.filter(pk=instance.pk).update(designation="Candidate")
-
         elif instance.role == 'employer' and not instance.designation:
             User.objects.filter(pk=instance.pk).update(designation="Employer")
 
         Token.objects.create(user=instance)
 
-        print(f"New user created: {instance.username} ({instance.role})")
+        send_welcome_email.delay(instance.email, instance.username)
