75e862fa994db1b208bca296b4f723b1136dfc41
Athika Priya <athikapriya1997@gmail.com>
2026-04-06 01:46:52 +0600

    Fix user post_save signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,19 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-
 from .models import User
-
-
 from .tasks import send_welcome_email
+
 
 @receiver(post_save, sender=User)
 def user_created_handler(sender, instance, created, **kwargs):
     if created:
         if instance.role == 'candidate' and not instance.designation:
-            User.objects.filter(pk=instance.pk).update(designation="Candidate")
+            instance.designation = "Candidate"
         elif instance.role == 'employer' and not instance.designation:
-            User.objects.filter(pk=instance.pk).update(designation="Employer")
+            instance.designation = "Employer"
+        instance.save(update_fields=['designation'])
 
         Token.objects.create(user=instance)
 
